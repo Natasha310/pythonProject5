71a4a33a36162cb2dac4500bb81b719af0b73647
Natasha310 <60444364+Natasha310@users.noreply.github.com>
2023-03-23 20:47:46 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import this
+print(this)
+git config --global user.email "seregavnan123@gmail.com"
+git config --global user.name "Natasha310"
+git remote add origin https://github.com/Natasha310/my_new_reposit.git
+
+four = [four for four in words if len(four) > 4]
+print(four)
+number_words = len(four)
+print(number_words)
